d3d191ed315f56f695ec692be113368292b76c7b
Mashiur Rahman <55910394+mashiurr@users.noreply.github.com>
2025-10-03 20:52:47 +0600

    Update 01-startup-check-master-nodes-uncordon.sh

--- 00-master-node/01-startup-check-container/01-startup-check-master-nodes-uncordon.sh
+++ 00-master-node/01-startup-check-container/01-startup-check-master-nodes-uncordon.sh
@@ -4,11 +4,17 @@ date
 set -e
 
 echo "This is Container Master-Nodes Kubernetes Cluster check up bash script!"
+
+# Pause before next step
+read -p "Press [Enter] to continue to [STEP 1] Check Kubernetes All Nodes Status"
 echo "Check Kubernetes All Nodes Status"
 kubectl get nodes -o wide
 
 echo "Check Kubernetes All Pods, Service & Namespace Status"
 kubectl get pod,svc,namespace -A -o wide
+
+# Pause before next step
+read -p "Press [Enter] to continue to [STEP 2] Uncordon Master-Node Controller 1st" 
 
 echo "Uncordon Master-Node Controller 1st" 
 kubectl uncordon master-node
@@ -22,6 +28,8 @@ kubectl get pods -n calico-system
 echo "Check All pods Label Named as calico-node"
 kubectl get pods -l k8s-app=calico-node -A
 
+# Pause before next step
+read -p "Press [Enter] to continue to [STEP 3] Watch Calico Pods, Services & TigerasStatus in Calico-System Namespace"
 echo "Watch Calico Pods, Services & TigerasStatus in Calico-System Namespace"
 #watch kubectl get tigerastatus
 watch kubectl get pods,svc,tigerastatus -n calico-system
