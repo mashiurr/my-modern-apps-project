1ef363f0af83d209e1d4921b203adbc7c0e7edbf
Mashiur Rahman <55910394+mashiurr@users.noreply.github.com>
2025-10-03 21:02:08 +0600

    Update 00-startup-check-master-nodes-check-up.sh

--- 00-master-node/01-startup-check-container/00-startup-check-master-nodes-check-up.sh
+++ 00-master-node/01-startup-check-container/00-startup-check-master-nodes-check-up.sh
@@ -4,22 +4,24 @@ echo "This is Container Master-Nodes check up bash script!"
 date
 set -e
 
+# Pause before next step
+read -p "Press [Enter] to continue to [Step 1] Master-Nodes check up: For “containerd” Installed Nodes:"..."
 echo "Master-Nodes check up: For “containerd” Installed Nodes:"
 sudo systemctl enable containerd && sudo systemctl start containerd &&  sudo systemctl status containerd
 
 # Pause before next step
-read -p "Press [Enter] to continue to Master-Nodes check up: For Kubernetes “kubeadm” Installed Nodes:..."
-
+read -p "Press [Enter] to continue to [Step 2] Master-Nodes check up: For Kubernetes “kubeadm” Installed Nodes:..."
 echo "Master-Nodes check up: For Kubernetes “kubeadm” Installed Nodes:"
 #kubeadm version && sudo cat /etc/modules-load.d/containerd.conf && sudo cat /etc/sysctl.d/kubernetes.conf && sudo cat /etc/hosts
 
-kubeadm version  && sudo cat /etc/sysctl.d/k8s.conf && sudo cat /etc/hosts && sudo sysctl --system
+kubeadm version && sudo cat /etc/sysctl.d/k8s.conf && sudo cat /etc/hosts && sudo sysctl --system
+
 
 #echo "For Master-Node only “containerd” based: Check Kubelet, containerd-daemon & Kubeadm Configuration"
 #sudo cat /etc/default/kubelet && sudo cat /etc/containerd/daemon.json && sudo cat /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 # Pause before next step
-read -p "Press [Enter] to continue to Master-Nodes check up: Restart systemctl daemon, Conatinerd & Kublet..."
+read -p "Press [Enter] to continue to [Step 3] Master-Nodes check up: Restart systemctl daemon, Conatinerd & Kublet..."
 
 echo "Restart systemctl daemon, Conatinerd & Kublet"
 
