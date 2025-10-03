5a866cf8e4ff87bad90dc8b0f2e4b0a7d70db2ff
Mashiur Rahman <55910394+mashiurr@users.noreply.github.com>
2025-10-03 20:41:08 +0600

    Update 03-k8s-init-cluster-with-Pod-CIDR-master-node.sh

--- 00-master-node/00-k8-cluster-deployment/00-masrter-node/03-k8s-init-cluster-with-Pod-CIDR-master-node.sh
+++ 00-master-node/00-k8-cluster-deployment/00-masrter-node/03-k8s-init-cluster-with-Pod-CIDR-master-node.sh
@@ -7,6 +7,9 @@ set -e
 
 echo "[STEP 1] Init cluster with Pod CIDR 192.168.0.0/16 at master-node" 
 cd /
+
+# Pause before next step
+read -p "Press [Enter] to continue to STEP 1:  Init cluster with Pod CIDR 192.168.0.0/16 at master-node..."
 sudo kubeadm init --control-plane-endpoint=master-node --pod-network-cidr=192.168.0.0/16 --upload-certs
 
 echo "[Completed] k8s-kubeadm cluster initiation for Master Node " 
