9e629516277d870316772a92b5aa4e398fcb0ce6
Mashiur Rahman <55910394+mashiurr@users.noreply.github.com>
2025-10-03 20:41:47 +0600

    Update 04-k8s-configure-kubectl-for-master-node.sh

--- 00-master-node/00-k8-cluster-deployment/00-masrter-node/04-k8s-configure-kubectl-for-master-node.sh
+++ 00-master-node/00-k8-cluster-deployment/00-masrter-node/04-k8s-configure-kubectl-for-master-node.sh
@@ -3,10 +3,14 @@
 set -e
 
 # k8s-Configure kubectl for Master Node
+# Pause before next step
+read -p "Press [Enter] to continue to STEP 1:"
 echo "[STEP 1] k8s-Configure kubectl for master-node" 
 
 mkdir -p $HOME/.kube
+
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 read -p "Press Enter to finis" input
